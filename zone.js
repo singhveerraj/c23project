2b5042a86882614fb008516d98a5e24798d01d11
singhveerraj <72805099+singhveerraj@users.noreply.github.com>
2020-11-04 13:59:35 +0530

    Add files via upload

---
+++ zone.js
@@ -0,0 +1,30 @@
+class Zone{
+    constructor(x,y,width,height){
+        options={
+            isStatic = true
+
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+
+        World.add(world,this.body);
+    }
+   
+   display(){
+       var pos = this.body.position;
+        
+       rectMode(CENTER);
+       rect(pos.x,pos.y,this.width,this.height);
+    
+   
+   
+   
+    }
+
+
+
+
+
+
+}
